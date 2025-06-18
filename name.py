9466cbea2f81864ec399150bc333e1d8fc0ea75e
Chandana Inguva <chandanainguva@gmail.com>
2025-06-18 14:53:56 +0530

    f strings

--- name.py
+++ name.py
@@ -2,3 +2,7 @@ name = 'ada lovelace'
 print(name.title())
 print(name.upper())
 print(name.lower())
+first_name = 'Ada'
+last_name = 'Lovelace'
+full_name = f"{first_name} {last_name}"
+print(f'Hello, {full_name.title()}')
