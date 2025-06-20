b8c605fa16a4dc7c6ecfeeb8c1fb4befae07ca0a
Chandana Inguva <chandanainguva@gmail.com>
2025-06-20 09:41:26 +0530

    quotes

---
+++ apostrophe.py
@@ -0,0 +1,4 @@
+#message = 'One of the Python's strength is its community' -- Will throw error because of unterminated string
+
+message = "One of the Python's strength is its community"
+print(message)
