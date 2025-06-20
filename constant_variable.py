83fd9f37c79ab3cccad350ed95aaf53bcabb6d58
Chandana Inguva <chandanainguva@gmail.com>
2025-06-20 11:44:59 +0530

    Chepter 2 done

---
+++ constant_variable.py
@@ -0,0 +1,6 @@
+CONSTANT_VARIABLE = 500
+print(CONSTANT_VARIABLE)
+CONSTANT_VARIABLE = 100
+print(CONSTANT_VARIABLE)
+'''Though we can change it we write in capital letters to indicate
+ that the value should be treated as constant and never be changed'''
---
+++ favorite_number.py
@@ -0,0 +1,2 @@
+favorite_number = 89
+print(f'My favorite number is : {favorite_number}')
